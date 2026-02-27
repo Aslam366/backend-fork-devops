11117b2837c4640fe4151359e1a9dc1b5226b820
Aslam366 <aslam@zetasoftwares.com>
2026-02-27 16:20:34 +0530

    as

---
+++ Controllers/azureTestController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ZetaSaasHRMSBackend.Authorization;
+using ZetaSaasHRMSBackend.Repository.Employee;
+
+namespace ZetaSaasHRMSBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class azureTestController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private const long MENU_ID = 7;
+
+        public azureTestController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpGet("Get")]
+        [HasPermission(MENU_ID, PermissionType.View)]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _employeeRepository.GetAllAsync());
+        }
+    }
+}
